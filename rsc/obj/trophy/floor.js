904a4d3450381b16183112ee80cc655456cad09a
bae43 <bae43@cornell.edu>
2014-12-12 04:51:24 -0500

    Added trophy model and files

---
+++ rsc/obj/trophy/floor.js
@@ -0,0 +1,42 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.63 Exporter",
+		"vertices"      : 4,
+		"faces"         : 1,
+		"normals"       : 1,
+		"colors"        : 0,
+		"uvs"           : [],
+		"materials"     : 0,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [],
+
+	"vertices" : [40.7303,0.086806,35.8446,-37.7943,0.086806,35.8446,40.7303,0.0868025,-42.6801,-37.7943,0.0868025,-42.6801],
+
+	"morphTargets" : [],
+
+	"normals" : [0,1,0],
+
+	"colors" : [],
+
+	"uvs" : [[]],
+
+	"faces" : [33,1,0,2,3,0,0,0,0],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+
+
+}
